8e7d74c0cd26c62ff9b91ae5135041a53e8be755
priesterbd <priesterbd@g.cofc.edu>
2020-03-04 14:37:07 -0500

    Fixed bug in runOpteamal.bat

--- opteamal_app/runOpteamal.bat
+++ opteamal_app/runOpteamal.bat
@@ -1,2 +1,2 @@
-start powershell cd opteamal_be/opteamal; ./venv/scripts/activate; python manage.py runserver
+start powershell cd opteamal_be/opteamal; ./venv/scripts/activate.bat; python manage.py runserver
 start powershell cd opteamal_fe/opteamal; npm start
